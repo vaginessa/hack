dac4a93116e575016e58f7f911eedefa709fd334
Youssef Awad <shadowblade7536@gmail.com>
2017-11-07 18:12:44 +0400

    idk

--- hack.js
+++ hack.js
@@ -8,3 +8,18 @@ TODO:
 - Internet connection detection
 - more...
 */
+
+//Dependencies
+const ArgParse = require('argparse').ArgumentParser;
+
+//Argparse Init
+var parser = new ArgParse({
+  version: '0.0.1',
+  addHelp: true,
+  description: 'Hack.js' //TODO: CHANGEME
+})
+
+//Argparse Arguments
+parser.addArgument(['-t', '--target'], {
+  help: "The target you want to hack."
+})
